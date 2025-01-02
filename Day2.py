c6004573a2d35a60fc926973c4eb052a8c778f39
simrannakarmi <nakarmisimran@gmail.com>
2025-01-02 23:54:32 +0545

    Day 2 commit

---
+++ Day2.py
@@ -0,0 +1,7 @@
+radius = float(input("Enter the radius of the circle: "))
+
+area = 3.14 * radius ** 2
+circum = 2 * 3.14 * radius
+
+print(f"Area of Cirlce: {area}")
+print(f"Circumference of Cirlce: {circum}")
