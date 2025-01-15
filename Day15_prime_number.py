1e5d12a5a99edb8c231f361042c1c6b55e221d16
simrannakarmi <nakarmisimran@gmail.com>
2025-01-15 22:14:57 +0545

    Day 15 commit

---
+++ Day15_prime_number.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+          
+if num == 1:
+    print("1 is not a Prime Number.")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print(f"{num} is not a Prime Number.")
+            break
+    else:
+        print(f"{num} is a Prime Number.")
+            
