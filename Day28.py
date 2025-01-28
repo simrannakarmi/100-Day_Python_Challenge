04826b70bbf1be69640cfe4a9b017aede64d47fb
simrannakarmi <nakarmisimran@gmail.com>
2025-01-28 23:55:11 +0545

    Day 28 commit

---
+++ Day28.py
@@ -0,0 +1,16 @@
+# Print:
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+
+num = int(input("Enter number of rows: "))
+for i in range(1, num+1):
+    for j in range(num-i):
+        print(" ", end="")
+        
+    for k in range(2*i - 1):
+        print("*", end="")
+        
+    print()
