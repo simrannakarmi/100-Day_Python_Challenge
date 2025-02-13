98a46a71ff3cbad1a3b910297cd514779aa1f181
simrannakarmi <nakarmisimran@gmail.com>
2025-02-14 00:15:01 +0545

    Day 44  commit

--- Day44_string_palindrome.py
+++ Day44_string_palindrome.py
@@ -1,17 +1,16 @@
 text = input("Enter a String: ")
 reverse = ""
 n = len(text)
-
+is_palindrome = True
 
 for i in range(0, n//2):
     # reverse += text[i]
     if text[i] != text[n-1]:
-        print("It is not a Palindrome")
+       is_palindrome = False
     n -= 1
+        
     
-    
-    
-if text == reverse:
+if is_palindrome:
     print("It is a Palindrome.")
 else:
     print("It is not a Palindrome")
