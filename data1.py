3df397920ed5f4afff373a8328dd9bab82646395
simrannakarmi <nakarmisimran@gmail.com>
2025-03-19 23:59:52 +0545

    Day 71 commit

--- data1.py
+++ data1.py
@@ -1,2 +1,12 @@
 import mysql.connector
 con = mysql.connector.connect(host="localhost", user="root", password="Password123", database="hundred_day")
+
+if con.is_connected():
+    q = "SELECT * FROM student"
+    cur = con.cursor()
+    cur.execute(q)
+    res = cur.fetchall()
+    for row in res:
+        print(row)
+else:
+    print("Error in Connection")
