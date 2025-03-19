946a4fde33f3e4c5e9c1c87c25aed33a0d6ab2c6
simrannakarmi <nakarmisimran@gmail.com>
2025-03-20 00:09:16 +0545

    Day 71 commit

--- Day71_mysql_show_table_data.py
+++ Day71_mysql_show_table_data.py
@@ -1,2 +1,16 @@
 import mysql.connector
-con = mysql.connector.connect(host="localhost", user="root", password="")
+con = mysql.connector.connect(host="localhost", user="root", password="Password123", database="hundred_day")
+
+if con.is_connected():
+    q = "SELECT * FROM student"
+    cur = con.cursor()
+    cur.execute(q)
+    res = cur.fetchall()
+    for row in res:
+        print("ID is",row[0]," Name is", row[1], " Age is",row[2])
+    if cur.rowcount == 0:
+        print("Table is Empty")
+    else:
+        print(cur.rowcount, " Students Processed")
+else:
+    print("Error in Connection")
